80b45d980d88b581bec013f30df29890c98951c8
zearkiatos <zearkiatos@users.noreply.github.com>
2021-10-21 02:42:53 +0000

    deploy: 67e5674bc4310aae9e78239878df07037ff52112

--- scripts/MediaPlayer.js
+++ scripts/MediaPlayer.js
@@ -1,49 +1,51 @@
-function MediaPlayer(configuration) {
-  this.media = configuration.element;
-  this.plugins = configuration.plugins || [];
+class MediaPlayer {
+  constructor(configuration) {
+    this.media = configuration.element;
+    this.plugins = configuration.plugins || [];
 
-  this.initializationPlugins();
+    this.initializationPlugins();
+  }
+  initializationPlugins() {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      media: this.media,
+      get muted() {
+        return this.media.muted;
+      },
+      set muted(mute) {
+        this.media.muted = mute;
+      }
+    };
+    this.plugins.forEach((plugin) => {
+      plugin.run(player);
+    });
+  }
+  play() {
+    this.media.paused ? this.media.play() : this.media.pause();
+  }
+  pause() {
+    this.media.pause();
+  }
+  togglePlay() {
+    this.media.paused ? this.play() : this.pause();
+  }
+  mute() {
+    this.media.muted = true;
+  }
+  unmute() {
+    this.media.muted = false;
+  }
+  toggleMute() {
+    this.media.muted ? this.unmute() : this.mute();
+  }
 }
 
-MediaPlayer.prototype.initializationPlugins = function () {
-  const player = {
-    play: () => this.play(),
-    pause: () => this.pause(),
-    media: this.media,
-    get muted() {
-      return this.media.muted;
-    },
-    set muted(mute) {
-      this.media.muted = mute;
-    }
-  };
-  this.plugins.forEach((plugin) => {
-    plugin.run(player);
-  });
-};
 
-MediaPlayer.prototype.play = function () {
-  this.media.paused ? this.media.play() : this.media.pause();
-};
 
-MediaPlayer.prototype.pause = function () {
-  this.media.paused ? this.media.play() : this.media.pause();
-};
 
-MediaPlayer.prototype.togglePlay = function () {
-  this.media.paused ? this.play() : this.pause();
-};
 
-MediaPlayer.prototype.mute = function () {
-  this.media.muted = true;
-};
 
-MediaPlayer.prototype.unmute = function () {
-  this.media.muted = false;
-};
 
-MediaPlayer.prototype.toggleMute = function () {
-  this.media.muted ? this.unmute() : this.mute();
-};
 
 export default MediaPlayer;
