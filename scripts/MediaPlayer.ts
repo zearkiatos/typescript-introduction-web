6d6e993efdb5d1baa920a01576d6d78ba1447ebb
Pedro Capriles <caprilespe@outlook.com>
2021-10-30 13:44:48 -0300

    Implementing ads pluging deployment in the display

--- scripts/MediaPlayer.ts
+++ scripts/MediaPlayer.ts
@@ -1,11 +1,18 @@
 class MediaPlayer {
   public media: HTMLMediaElement;
   private plugins: Array<any>;
+  public container: HTMLElement;
   constructor(configuration) {
     this.media = configuration.element;
     this.plugins = configuration.plugins || [];
-
     this.initializationPlugins();
+    this.initializationPlayer();
+  }
+  initializationPlayer() {
+    this.container = document.createElement('div');
+    this.container.style.position = "relative";
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media);
   }
   private initializationPlugins() {
     this.plugins.forEach((plugin) => {
