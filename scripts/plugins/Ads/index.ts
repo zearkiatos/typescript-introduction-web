498f8b29f97b529ee8eb65172336ae4e35ad8334
zearkiatos <zearkiatos@users.noreply.github.com>
2021-10-30 17:07:41 +0000

    deploy: 6d6e993efdb5d1baa920a01576d6d78ba1447ebb

--- scripts/plugins/Ads/index.ts
+++ scripts/plugins/Ads/index.ts
@@ -6,12 +6,15 @@ class AdsPlugin {
     private player: MediaPlayer;
     private media: HTMLMediaElement;
     private currentAd: IAd;
+    private adsContainer: HTMLElement;
     constructor() {
         this.ads = Ads.getInstance();
+        this.adsContainer = document.createElement('div');
         this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
     }
     run(player: MediaPlayer) {
         this.player = player;
+        this.player.container.appendChild(this.adsContainer);
         this.media = this.player.media;
         this.media.addEventListener('timeupdate', this.handleTimeUpdate);
     }
@@ -30,7 +33,22 @@ class AdsPlugin {
         }
         const ad = this.ads.getAd();
         this.currentAd = ad;
-        console.log(ad);
+        this.adsContainer.innerHTML = `
+            <div class="ads">
+                <a class="ads__link" href="${this.currentAd.url}" target="_blank">
+                    <img class="ads__img" src="${this.currentAd.imageUrl}" />
+                    <div class="ads__info">
+                        <h5 class="ads__title">${this.currentAd.title}</h5>
+                        <p class="ads__body">${this.currentAd.body}</p>
+                    </div>
+                </a>
+            </div>
+        `;
+
+        setTimeout(() => {
+            this.currentAd = null;
+            this.adsContainer.innerHTML = '';
+        }, 10000);
     }
 }
 
